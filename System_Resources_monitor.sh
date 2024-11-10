891ac2de88a7a7cfe86dc6174e0512f17a4b3197
micheal-ndoh <michaelndoh9@gmail.com>
2024-11-10 17:21:45 +0100

    Modified and Improved Guessing game to be able to take ranges and add difficulties

--- System_Resources_monitor.sh
+++ System_Resources_monitor.sh
@@ -3,7 +3,6 @@ THRESHOLD=100000  # 100MB in KB
 while true; do
     FREE_MEM=$(free | grep Mem | awk '{print $4}')
     if [ "$FREE_MEM" -lt "$THRESHOLD" ]; then
-        echo "Memory low: $(date)" | mail -s "Memory Alert" admin@example.com
-    fi
-    sleep 600  # Check every 10 minutes
+        echo "Memory low: $(date)" 
+fi
 done
