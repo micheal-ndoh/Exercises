57d362d5c547bea40c23d1d6582436e75bc6a392
micheal-ndoh <michaelndoh9@gmail.com>
2024-11-12 12:01:18 +0100

    Added new feature to show score and loss as games progresses and also a new fun mode

--- System_Resources_monitor.sh
+++ System_Resources_monitor.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 THRESHOLD=100000  # 100MB in KB
 while true; do
-    FREE_MEM=$(free | grep Mem | awk '{print $4}')
+    FREE_MEM=$(free | grep Mem | awk '{print}')
     if [ "$FREE_MEM" -lt "$THRESHOLD" ]; then
-        echo "Memory low: $(date)" 
-fi
+        echo "Memory low: $(date)" | mail -s "Memory Alert" michaelndoh9@gmail.com.com
+    fi
+    sleep 60  # Check every 10 minutes
 done
